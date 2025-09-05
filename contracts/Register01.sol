697eeee881b0d0690036939bb7f98be6500fc6d6
erik19borgnia <erik19borgnia@gmail.com>
2025-09-05 15:20:14 -0300

    ....GIT DE M....

---
+++ contracts/Register01.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Register01 {
+    string private storedInfo;
+
+    function setInfo(string memory myInfo) external {
+        storedInfo = myInfo;
+    }
+
+    function getInfo() external view returns (string memory) {
+        return storedInfo;
+    }
+}
