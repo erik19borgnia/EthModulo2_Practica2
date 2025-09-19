0a6ed6d14bbfad245da5116bf67eee4f2864bef9
erik19borgnia <erik19borgnia@gmail.com>
2025-09-19 15:34:28 -0300

    Segunda versión de ToDoList

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -20,50 +20,60 @@ contract ToDoList {
         uint256 createTime;
         Estado state;
     }
-    uint lastID=0;
     // Array dinámico para almacenar las tareas
     Task[] private s_tasks;
 
 
 
-    event TareaCreada(string content, uint256 createTime);
-    event TareaEmpezada(uint id, uint256 startTime);
-    event TareaCompletada(uint id, uint256 startTime);
+    event TareaCreada(Task task, uint256 createTime);
+    event TareaEmpezada(Task task, uint256 startTime);
+    event TareaCompletada(Task task, uint256 startTime);
+    event TareaEliminada(string content, uint256 deleteTime);
 
 
     // Función para agregar una nueva tarea a la lista
-    function addTask(string memory _content) public {
+    function addTask(string calldata _content) public {
         Task memory newTask = Task(_content, block.timestamp, Estado.Pendiente);
         s_tasks.push(newTask);
 
-        emit TareaCreada(_content, block.timestamp);
-
-        lastID++;
+        emit TareaCreada(newTask, block.timestamp);
     }
 
     // Función para marcar como iniciada una tarea
     function startTask(uint _id) public {
-        require(_id < lastID, "START - La tarea con ese id no existe!");
+        require(_id < s_tasks.length, "START - La tarea con ese id no existe!");
         s_tasks[_id].state = Estado.EnProceso;
-        emit TareaEmpezada(_id, block.timestamp);
+        emit TareaEmpezada(s_tasks[_id], block.timestamp);
     }
 
     // Función para marcar como completada una tarea
     function completeTask(uint _id) public {
-        require(_id < lastID, "COMPLETE - La tarea con ese id no existe!");
+        require(_id < s_tasks.length, "COMPLETE - La tarea con ese id no existe!");
         s_tasks[_id].state = Estado.Completada;
-        emit TareaCompletada(_id, block.timestamp);
+        emit TareaCompletada(s_tasks[_id], block.timestamp);
     }
 
     // Función para ver la cantidad de tareas creadas
     function getTaskCount() public view returns (uint) {
-        return lastID;
+        return s_tasks.length;
     }
 
     // Función para ver el contenido de una tarea
     function getTaskContent(uint _id) public view returns (Task memory) {
-        require(_id < lastID, "CONTENT - La tarea con ese id no existe!");
+        require(_id < s_tasks.length, "CONTENT - La tarea con ese id no existe!");
         return s_tasks[_id];
+    }
+
+    // Función que elimina una tarea a partir de su descripción
+    function deleteTask(string calldata _content) public {
+        uint i = 0;
+        while (i<s_tasks.length && keccak256(abi.encodePacked(_content)) != keccak256(abi.encodePacked(s_tasks[i].content))) {
+            i++;
+        }
+        require(i < s_tasks.length, "DELETE - La tarea con ese contenido no existe!");
+        s_tasks[i] = s_tasks[s_tasks.length - 1];
+        s_tasks.pop();
+        emit TareaEliminada(_content, block.timestamp);
     }
     
 }
